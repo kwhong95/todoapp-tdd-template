890794cf4333b947794103d4da6cc76aa8343741
kwhong95 <kwhong95@kakao.com>
2022-04-06 16:41:10 +0900

    Create ToDo-List Test Code

---
+++ src/components/ToDoList/index.test.tsx
@@ -0,0 +1,54 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import 'jest-styled-components';
+
+import { ToDoListProvider } from 'contexts';
+import { TodoList } from '.';
+
+describe('<ToDoList />', () => {
+  it('renders component correctly', () => {
+    const { container } = render(
+      <ToDoListProvider>
+        <TodoList />
+      </ToDoListProvider>,
+    );
+
+    const toDoList = screen.getByTestId('toDoList');
+    expect(toDoList).toBeInTheDocument();
+    // eslint-disable-next-line testing-library/no-node-access
+    expect(toDoList.firstChild).toBeNull();
+
+    expect(container).toMatchSnapshot();
+  });
+
+  it('shows toDo list', () => {
+    localStorage.setItem('ToDoList', '["ToDo 1", "ToDo 2", "ToDo 3"]');
+
+    render(
+      <ToDoListProvider>
+        <TodoList />
+      </ToDoListProvider>,
+    );
+
+    expect(screen.getByText('ToDo 1')).toBeInTheDocument();
+    expect(screen.getByText('ToDo 2')).toBeInTheDocument();
+    expect(screen.getByText('ToDo 3')).toBeInTheDocument();
+    expect(screen.getAllByText('삭제').length).toBe(3);
+  });
+
+  it('deletes toDo item', () => {
+    localStorage.setItem('ToDoList', '["ToDo 1", "ToDo 2", "ToDo 3"]');
+
+    render(
+      <ToDoListProvider>
+        <TodoList />
+      </ToDoListProvider>,
+    );
+
+    const toDoItem = screen.getByText('ToDo 2');
+    expect(toDoItem).toBeInTheDocument();
+    // eslint-disable-next-line testing-library/no-node-access
+    fireEvent.click(toDoItem.nextElementSibling as HTMLElement);
+    expect(toDoItem).not.toBeInTheDocument();
+    expect(JSON.parse(localStorage.getItem('ToDoList') as string)).not.toContain('ToDo 2');
+  });
+});
